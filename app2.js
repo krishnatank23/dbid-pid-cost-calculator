d928dcadb5c8593c0e5b71e600224514a06d52dc
unknown <you@example.com>
2026-06-12 16:20:50 +0530

    change date formate

--- app2.js
+++ app2.js
@@ -154,8 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function formatDateLong(date) {
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        return `${months[date.getUTCMonth()]} ${date.getUTCDate()}, ${date.getUTCFullYear()}`;
+        return formatDateDDMMYYYY(date);
     }
 
     function formatDateDDMMYYYY(date) {
